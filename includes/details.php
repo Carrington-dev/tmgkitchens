0d6fed9b26753c804d8fb9d327cca223ffb1a1bd
Carrington-dev <crn96m@gmail.com>
2025-01-02 19:31:59 +0200

    Welcoming Services

--- includes/details.php
+++ includes/details.php
@@ -1,10 +1,10 @@
 <?php 
-  $company = "Rendy Pools";
+  $company = "TMG Kitchens & Log Homes";
   $page = "About";
   $tel = "+27 79 800 5807";
 //   $tel = "+27 61 431 9286";
   $adminmail = "crn96m@gmail.com";
-  $email = "info@rendypoolservices.co.za";
+  $email = "info@tmgkitchens.co.za";
   $replytomail = $email;
   $address = "395 Francis Baard Street</p><p>Pretoria, Gauteng 0001";
   $copyright = "<p>Copyright &copy; 2014-" . date("Y") . " ". $company . '. All rights reserved.';
--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 <html lang="en">
   <?php include 'includes/details.php';?>
 
-  
-  
+    
   <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Pool Service Company | <?php echo "$company"; ?></title>
+    <title><?php echo "$page"; ?> | <?php echo "$company"; ?></title>
     <meta name="description" content="">
     <meta name="keywords" content="">
 
@@ -50,7 +49,7 @@
           <img src="assets/img/hero-carousel/hero-carousel-1.jpg" alt="">
           <div class="container">
             <h2>We are professional</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Turn your dream home into reality. Perfect finishes, custom furniture, and details that make a difference.</p>
             <a href="#featured-services" class="btn-get-started">Get Started</a>
           </div>
         </div><!-- End Carousel Item -->
@@ -58,8 +57,8 @@
         <div class="carousel-item">
           <img src="assets/img/hero-carousel/hero-carousel-2.jpg" alt="">
           <div class="container">
-            <h2>At vero eos et accusamus</h2>
-            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut.</p>
+            <h2>A legacy of greatly successful projects</h2>
+            <p>We don't just renovate spaces, we transform lifestyles. Explore our portfolio and witness the magic we create.</p>
             <a href="#featured-services" class="btn-get-started">Get Started</a>
           </div>
         </div><!-- End Carousel Item -->
@@ -67,8 +66,8 @@
         <div class="carousel-item">
           <img src="assets/img/hero-carousel/hero-carousel-3.jpg" alt="">
           <div class="container">
-            <h2>Temporibus autem quibusdam</h2>
-            <p>Beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt omnis iste natus error sit voluptatem accusantium.</p>
+            <h2>Modern Gloss Black Kitchen</h2>
+            <p>Embrace sophistication with a modern gloss black kitchen. The sleek, high-shine cabinets exude drama and reflect light, creating a sense of spaciousness. This contemporary look is perfect for those who love a bold and polished aesthetic.</p>
             <a href="#featured-services" class="btn-get-started">Get Started</a>
           </div>
         </div><!-- End Carousel Item -->
