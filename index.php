c495ecea76565a059d35f5cb576b95641f824579
Carrington-dev <crn96m@gmail.com>
2025-01-02 19:35:08 +0200

    Added Consultation Services

--- index.php
+++ index.php
@@ -93,11 +93,11 @@
 
         <div class="row" data-aos="zoom-in" data-aos-delay="100">
           <div class="col-xl-9 text-center text-xl-start">
-            <h3>Call To Action</h3>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Free Consultation</h3>
+            <p>A no obligation consultation provides the opportunity to discuss your project and see how we can work  together.</p>
           </div>
           <div class="col-xl-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="#">Call To Action</a>
+            <a class="cta-btn align-middle" href="#">Consult Now</a>
           </div>
         </div>
 
